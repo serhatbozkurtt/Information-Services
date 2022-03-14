0dc0531274e9242e8ab865a851d33948163f6979
serhatbozkurtt <65898491+serhatbozkurtt@github.com>
2022-03-14 17:27:45 +0300

    initial commit

---
+++ bist.py
@@ -0,0 +1,57 @@
+import base64, requests, sys, json
+import requests
+import json
+import datetime
+
+client_id = "l7xx052c5c1953a141d09d7ceabb221aa757"
+client_secret = "f78b4311a28b4a56a901e06383e79634"
+
+# Encode the client ID and client secret
+authorization = base64.b64encode(bytes(client_id + ":" + client_secret, "ISO-8859-1")).decode("ascii")
+
+
+headers = {
+    "Authorization": f"Basic {authorization}",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+body = {
+    "grant_type": "client_credentials"
+}
+
+response = requests.post("https://apigw.vakifbank.com.tr:8443/auth/oauth/v2/token", data=body, headers=headers)
+
+#print(response.text)
+
+a = json.loads(response.text)
+
+print(a['access_token'])
+
+access_token = a['access_token']
+
+url = "https://apigw.vakifbank.com.tr:8443/getBISTPrices"
+
+bugun = datetime.date.today()
+print(bugun)
+hour = datetime.datetime.now()
+hour = hour.hour
+minute = datetime.datetime.now()
+minute = minute.minute
+second = datetime.datetime.now()
+second = second.second
+
+tarih = str(bugun) + "T" + str(hour) + ":" + str(minute) + ":" + str(second) + "+03:00"
+print(tarih)
+
+payload = json.dumps({
+  "SessionDate": tarih
+})
+headers = {
+  'Authorization': "Bearer " + access_token,
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(json.dumps(response.json(), indent=4, sort_keys=True))
+
+
